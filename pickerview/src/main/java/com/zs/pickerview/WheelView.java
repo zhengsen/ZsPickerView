2d2d523c39b231f763c1387625398515d090783c
zhengsen <zhengsen09@gmail.com>
2016-07-06 19:59:28 +0800

    create module lib
    create module lib

---
+++ pickerview/src/main/java/com/zs/pickerview/WheelView.java
@@ -0,0 +1,22 @@
+package com.zs.pickerview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by zhengsen on 2016/7/6.
+ */
+public class WheelView extends View {
+    public WheelView(Context context) {
+        super(context);
+    }
+
+    public WheelView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WheelView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
